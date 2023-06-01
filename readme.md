dd22fd91b10820e49400df27d583278759f26c70
Ahmad A. Fouad <a.adel.dev@gmail.com>
2023-06-01 11:36:35 +0300

    styling menu page

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 Photo by <a href="https://unsplash.com/@louishansel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Louis Hansel</a> on <a href="https://unsplash.com/photos/wVoP_Q2Bg_A?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@covertnine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tim Toomey</a> on <a href="https://unsplash.com/photos/STqHLqMne3k?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@ferhadd?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Farhad Ibrahimzade</a> on <a href="https://unsplash.com/photos/M6L3x7rJdoE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@kobbymendez?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kobby Mendez</a> on <a href="https://unsplash.com/photos/idTwDKt2j2o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@whitney_wright?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Whitney Wright</a> on <a href="https://unsplash.com/photos/TgQkxQc-t_U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,74 @@
+import Appetizers from "./appetizers.jpg";
+import MainCourse from "./main-course.jpg";
+import Dessert from "./dessert.jpg";
+import Drinks from "./drinks.jpg";
+
+const appImage = new Image();
+appImage.src = Appetizers;
+
+const maincourseImage = new Image();
+maincourseImage.src = MainCourse;
+maincourseImage.class = "card-image";
+
+const dessertImage = new Image();
+dessertImage.src = Dessert;
+dessertImage.class = "card-image";
+
+const drinksImage = new Image();
+drinksImage.src = Drinks;
+drinksImage.class = "card-image";
+
 export function menuSection() {
   const pageContent = document.createElement("section");
 
-  pageContent.innerHTML = `
-    <div class="container">
-      <h2>Our Menu</h2>
-      <p>
-        Explore our diverse menu featuring a wide range of culinary delights:
-      </p>
-      <ul>
-        <li>Appetizers</li>
-        <li>Main Courses</li>
-        <li>Desserts</li>
-        <li>Drinks</li>
-      </ul>
-      <p>
-        Whether you're a vegetarian, a seafood lover, or a meat enthusiast, we
-        have something for everyone.
-      </p>
-      </div>`;
+  const title = document.createElement("h2");
+  title.innerHTML = "Our Menu";
+
+  const description = document.createElement("p");
+  description.innerHTML =
+    "Explore our diverse menu featuring a wide range of culinary delights";
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("cards-container");
+
+  for (let i = 0; i < 4; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    cardContainer.appendChild(card);
+  }
+
+  cardContainer.children[0].appendChild(appImage);
+  appImage.classList.add("card-image");
+  const appetizersDescription = document.createElement("p");
+  appetizersDescription.innerText = "Appetizers";
+  cardContainer.children[0].appendChild(appetizersDescription);
+
+  cardContainer.children[1].appendChild(maincourseImage);
+  maincourseImage.classList.add("card-image");
+  const mainCourseDescription = document.createElement("p");
+  mainCourseDescription.innerText = "Main Courses";
+  cardContainer.children[1].appendChild(mainCourseDescription);
+
+  cardContainer.children[2].appendChild(dessertImage);
+  dessertImage.classList.add("card-image");
+  const dessertsDescription = document.createElement("p");
+  dessertsDescription.innerText = "Desserts";
+  cardContainer.children[2].appendChild(dessertsDescription);
+
+  cardContainer.children[3].appendChild(drinksImage);
+  drinksImage.classList.add("card-image");
+  const drinksDescription = document.createElement("p");
+  drinksDescription.innerText = "Drinks";
+  cardContainer.children[3].appendChild(drinksDescription);
+
+  const outroText = document.createElement("p");
+  outroText.innerHTML =
+    "Whether you're a vegetarian, a seafood lover, or a meat enthusiast, we have something for everyone.";
+
+  pageContent.appendChild(title);
+  pageContent.appendChild(description);
+  pageContent.appendChild(cardContainer);
+  pageContent.appendChild(outroText);
 
   return pageContent;
 }
